af74d92b1f8dda42028130eb49e31ce62e156d19
Aditya Maheshwari <aditya.yow@gmail.com>
2025-08-06 12:24:38 -0400

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import tempfile
 from scripts.utils import read_and_parse_dates
 
 def test_date_parsing():
     df = pd.DataFrame({'date': ['2023-01-01']})
-    result = read_and_parse_dates(df, 'date')
+
+    with tempfile.NamedTemporaryFile(suffix=".csv", mode="w+", delete=False) as tmp:
+        df.to_csv(tmp.name, index=False)
+        result = read_and_parse_dates(tmp.name, 'date')
+
     assert pd.api.types.is_datetime64_any_dtype(result['date'])
 
